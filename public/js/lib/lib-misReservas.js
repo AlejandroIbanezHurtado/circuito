a7fdef17cb51bc1ea9e228aadfa3dd2ad882255b
Alejandro Ibáñez Hurtado <aibahur653@g.educaand.es>
2022-02-22 13:16:02 +0100

    v2.0

--- public/js/lib/lib-misReservas.js
+++ public/js/lib/lib-misReservas.js
@@ -5,14 +5,23 @@ $(function(){
         $("#spinner").remove();
         for(i=0;i<result.length;i++)
         {
-            contenedor = $("<div id='coche_"+result[i].id+"' class='contenedorCoches my-5'></div>")
+            contenedor = $("<div id='coche_"+result[i].id+"' class='contenedorCoches my-5'></div>");
             tituloPrinci = $("<div class='row align-items-center mb-4 ml-5'></div>");
             titulo = $("<div class='col-md-6'><h1 class='m-0'>"+result[i].marca+" "+result[i].modelo+"</h1><div>");
             fila = $("<div class='row'></div>");
-            imagen = $("<div class='col-lg-6'><div style='background-image: url(/bd/"+result[i].imagen+"); width: 100%; background-size: 70%; background-repeat: no-repeat; background-position: center; height:210px'></div></div>");
-            datos = $("<div class='col-lg-4'><p>Se reservó el día: "+result[i].fecha+"</p><p>Fecha de inicio: "+result[i].fecha_inicio+"</p><p>Fecha de fin: "+result[i].fecha_fin+"</p><h4>Precio: "+result[i].precio+"€</h4></div>");
-            botonCancelar = $("<div class='col-lg-2'><input type='button' value='Cancelar' data-bs-toggle='modal' data-bs-target='#exampleModal' class='btn btn-danger'></div>");
-            cajaPrinci.append(contenedor.append(tituloPrinci.append(titulo)).append(fila.append(imagen).append(datos).append(botonCancelar)));        
+            imagen = $("<div class='col-md-6 col-sm-6 col-6'><div style='background-image: url(/bd/"+result[i].imagen+"); width: 100%; background-size: 70%; background-repeat: no-repeat; background-position: center; height:210px'></div></div>");
+            datos = $("<div class='col-md-4 col-sm-4 col-4'><p>Se reservó el día: "+result[i].fecha+"</p><p>Fecha de inicio: "+result[i].fecha_inicio+"</p><p>Fecha de fin: "+result[i].fecha_fin+"</p><h4>Precio individual: "+result[i].precio_coche+"€</h4>Precio conjunto: "+result[i].precio+"€</h4></div>");
+            botonCancelar = $("<div class='col-md-2 col-sm-2 col-2'><input type='button' value='Cancelar' data-bs-toggle='modal' data-bs-target='#exampleModal' class='btn btn-danger'></div>");
+            cajaPrinci.append(contenedor.append(tituloPrinci.append(titulo)).append(fila.append(imagen).append(datos).append(botonCancelar)));
+            botonCancelar.find("input[type='button']").on("click",function(){
+                id_coche = $(this).parent().parent().parent().attr("id").split("_")[1];
+                $("#modalHora").modal("show");
+                $("#btnCancelar").on("click",function(){
+                    $.get("/api/borraReserva/"+id_coche,function(data){
+                        contenedor.remove();
+                    })
+                })
+            })        
         }
     })
 })
--- src/api/MisReservasController.php
+++ src/api/MisReservasController.php
@@ -3,6 +3,7 @@
 namespace App\api;
 
 use App\Entity\Reserva;
+use App\Entity\DetalleReserva;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -20,5 +21,20 @@ class MisReservasController extends AbstractController
         $correo = $_SESSION['_sf2_attributes']['_security.last_username'];
         $reservas = $repositoryCoche->buscarPorCorreo($correo);
         return new Response(json_encode($reservas));
+    }
+
+    /**
+     * @Route("/api/borraReserva/{id}", name="borrarReserva")
+     */
+    public function borrarReserva(ManagerRegistry $doctrine, $id): Response
+    {
+        $entityManager = $doctrine->getManager();
+        $repositoryReserva = $doctrine->getRepository(Reserva::class);
+        $repositoryDetalleReserva = $doctrine->getRepository(DetalleReserva::class);
+        $reserva = $repositoryDetalleReserva->findOneBy(['coche' => $id]);
+        $entityManager->remove($reserva);
+        $entityManager->flush();
+        // dd(json_encode($doctrine));
+        return new Response(json_encode($doctrine));
     }
 }
