91943a72ecc46f178f388ec2778376af30d76e42
Alejandro Ibáñez Hurtado <aibahur653@g.educaand.es>
2022-03-04 02:47:03 +0100

    v3.0

--- public/js/lib/lib-misReservas.js
+++ public/js/lib/lib-misReservas.js
@@ -11,6 +11,7 @@ $(function(){
                 result[i].modelo = "";
                 result[i].imagen = "../images/circuito.jpg";
                 result[i].id = result[i].reserva_id;
+                result[i].precio_coche = result[i].precio;
             }
             contenedor = $("<div id='coche_"+result[i].id+"' class='contenedorCoches my-5'></div>");
             if(result[i].id==null) contenedor.attr("circuito","true");
@@ -18,7 +19,7 @@ $(function(){
             titulo = $("<div class='col-md-6'><h1 class='m-0'>"+result[i].marca+" "+result[i].modelo+"</h1><div>");
             fila = $("<div class='row'></div>");
             imagen = $("<div class='col-md-6 col-sm-6 col-6'><div style='background-image: url(/bd/"+result[i].imagen+"); width: 100%; background-size: 70%; background-repeat: no-repeat; background-position: center; height:210px'></div></div>");
-            datos = $("<div class='col-md-4 col-sm-4 col-4'><p>Se reservó el día: "+result[i].fecha+"</p><p>Fecha de inicio: "+result[i].fecha_inicio+"</p><p>Fecha de fin: "+result[i].fecha_fin+"</p><h4>Precio individual: "+result[i].precio+"€</h4>Precio conjunto: "+result[i].precio+"€</h4></div>");
+            datos = $("<div class='col-md-4 col-sm-4 col-4'><p>Se reservó el día: "+result[i].fecha+"</p><p>Fecha de inicio: "+result[i].fecha_inicio+"</p><p>Fecha de fin: "+result[i].fecha_fin+"</p><h4>Precio individual: "+result[i].precio_coche+"€</h4>Precio conjunto: "+result[i].precio+"€</h4></div>");
             botonCancelar = $("<div class='col-md-2 col-sm-2 col-2' data-bs-toggle='modal' data-bs-target='#modalHora'><input type='button' value='Cancelar' class='btn btn-danger'></div>");
             cajaPrinci.append(contenedor.append(tituloPrinci.append(titulo)).append(fila.append(imagen).append(datos)));
             actual = new Date();
